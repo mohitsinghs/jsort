e1529f420f9241d8e683afcd291092d2a1d5b857
Mohit Singh <mohit@mohitsingh.in>
2021-12-14 14:24:36 +0530

    Bump deps

--- src/cli.rs
+++ src/cli.rs
@@ -6,25 +6,25 @@ pub fn parse() -> ArgMatches {
         .about(crate_description!())
         .args(&[
             Arg::new("input")
-                .about("files/directory to process")
+                .help("files/directory to process")
                 .index(1)
                 .required(true),
             Arg::new("replace")
                 .short('r')
                 .long("replace")
-                .about("sort and replace files")
+                .help("sort and replace files")
                 .required(false)
                 .takes_value(false),
             Arg::new("out")
                 .short('o')
                 .long("out")
-                .about("output directory ( ignores suffix )")
+                .help("output directory ( ignores suffix )")
                 .takes_value(true)
                 .required(false),
             Arg::new("suffix")
                 .short('s')
                 .long("suffix")
-                .about("suffix for output files")
+                .help("suffix for output files")
                 .default_value("sorted")
                 .required(false),
         ]);
